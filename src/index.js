1c37bd5d9b7c4d5eddb3babbba448ffb0125eef1
HosseinJarrahi <hj021hj@gmail.com>
2025-09-30 13:48:22 +0330

    a

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import MCrud from '@/components/MCrud.vue'
 import '@/assets/main.css'
 import '@/assets/tabulator.css'
 import install from './install'
-import { registerFields } from '@/helpers/formueCrud'
 import fields from '@/helpers/fields'
+import MCrud from '@/components/MCrud.vue'
+import * as emitter from '@/helpers/emitter'
+import { registerFields } from '@/helpers/formueCrud'
 import ActionButton from '@/components/ActionButton.vue'
 
-export { MCrud, registerFields, install, fields, ActionButton }
+export { MCrud, registerFields, install, fields, ActionButton, emitter }
